4819e5b49688f8c1b94d82927f58dc0f97901584
paulodeb <oyediranp@gmail.com>
2022-11-05 21:41:52 +0100

    Updated Changes

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.css'
 import Footer from './Footer'
 
 function Contact() {
+const [firstname, setFirstname] = useState('')
+const [lastname, setLastname] = useState('')
+const [email, setEmail] = useState('')
+const [textarea, setTextarea] = useState('')
+const [checkbox, setCheckbox] = useState(false)
+const [error, setError] = useState({})
+const  canSend = firstname !== '' && lastname !== '' && email !== '' && textarea !== '' && checkbox === true
+error.firstname = firstname === '' ? 'First name is required' : ''
+error.lastname = lastname === '' ? 'Last name is required' : ''
+error.email = email === '' ? 'This is a hint text to help user.' : ''
+error.textarea = textarea === '' ? 'Please enter a message' : ''
+error.checkbox = checkbox === false ? 'You must agree to the terms and conditions' : ''
   return (
     <>
 
@@ -18,37 +30,52 @@ function Contact() {
         <label id='first_name_label'>
             First name
         </label>
-        <input id='first_name' type="text" name="firstname" placeholder="Enter your first name"/>
-        
-        <p>This is a hint text to help users</p>
+        <input id='first_name' type="text" name="firstname" placeholder="Enter your first name"
+        value={firstname} onChange={(e) => setFirstname(e.target.value)}    
+        />
         </div>
         <div id='last_name_container'>
         <label id='last_name_label'> Last name </label>
-        <input id='last_name' type="text" name="lastname" placeholder="Enter your last name" />
+        <input id='last_name' type="text" name="lastname" placeholder="Enter your last name" 
+        value={lastname} onChange={(e) => setLastname(e.target.value)}
+        />
+        
         </div>
         </div>
         <div id='email_container'>
         <label id='email_label'> Email </label>    
-        <input id='email' type="email" name="email" placeholder="yourname@gmail.com"/>
+        <input id='email' type="email" name="email" placeholder="yourname@gmail.com"
+        value={email} onChange={(e) => setEmail(e.target.value)}
+        />
+        {error.email && <p id='hint'>{error.email}</p>}
         </div>
 
         <div id='message_container'>
         <label id='message_label'>Message </label>
-        <textarea id='message' name="message" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+        <textarea id='message' name="message" placeholder="Send me a message and I'll reply you as soon as possible..."
+        value={textarea} onChange={(e) => setTextarea(e.target.value)}
+        >
+        </textarea>
+            {error.textarea && <p className='error'>{error.textarea}</p>}
         </div>
 
         <div id='checkbox_container'>
-        <input id='checkbox_base' type= "checkbox" name="subscribe" value="subscribe"/>
+        <input id='checkbox_base' type= "checkbox" name="subscribe"
+         onClick={(e) => setCheckbox(e.target.checked)}
+            
+        />
 
-        <div id='text_checkbox_container'>
+        
 
         <label id='text_checkbox'>
         You agree to providing your data to Paul Oyediran who may contact you.
         </label>
         </div>
-        </div>
 
-        <button id='btn_submit' type="submit">Send message</button>
+        <button id='btn_submit' type="submit"
+         disabled={!canSend}
+
+         >Send message</button>
         </form>
     </div>
     </div>
